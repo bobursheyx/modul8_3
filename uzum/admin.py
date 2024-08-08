b8b46f78a88aed7ae1b03722ffb6285b29be5518
bobursheyx <olponovboburjon2@gmail.com>
2024-08-08 14:35:13 -0700

    meniki

---
+++ uzum/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+from uzum.models import Category, Product, Group, Comment, Image
+
+
+# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['title', 'image']
+    list_display = ['title', 'image', 'slug']
+    search_fields = ['title']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'price', 'group', 'discount', 'user_like']
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    fields = ['name', 'image', 'category']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['rating', 'product', 'user', 'comment']
+    readonly_fields = ['user']  # Optionally make the user field read-only
+
+    def save_model(self, request, obj, form, change):
+        if not change:  # Only set user on creation, not on updates
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+
+# Register the admin class with the Comment model
+admin.site.register(Comment, CommentAdmin)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['is_primary', 'image', 'product']
---
+++ uzum/views.py
@@ -0,0 +1,117 @@
+# Create your views here.
+from urllib import request
+
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, status
+from uzum.models import Category, Product, Group
+from uzum.serializers import CategorySerializer, ProductSerializer, GroupSerializer
+
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetail(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CreateCategoryView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class UpdateCategoryView(generics.UpdateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteCategoryView(generics.DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+
+    def get(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    #
+    def delete(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class GroupListView(generics.ListAPIView):
+    serializer_class = GroupSerializer
+    lookup_field = 'slug'
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('slug')
+        if category_slug:
+            return Group.objects.filter(category__slug=category_slug)
+        return Group.objects.all()
+
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'slug'
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('category_slug')
+        group_slug = self.kwargs.get('group_slug')
+
+        queryset = Product.objects.all()
+
+        if category_slug and group_slug:
+            queryset = queryset.filter(group__category__slug=category_slug, group__slug=group_slug)
+        elif category_slug:
+            queryset = queryset.filter(category__slug=category_slug)
+        elif group_slug:
+            queryset = queryset.filter(group__slug=group_slug)
+
+        return queryset
